dcd07c81509910203af6219c618b83a5328521ea
smernick <smernick3@gmail.com>
2016-03-21 17:43:22 -0400

    finished

--- code.js
+++ code.js
@@ -1,1 +1,23 @@
 'use strict';
+
+
+function favIceCream(argument) {
+  return "I love " + argument
+}
+
+function shouting(arg) {
+  var arg1 = arg.toUpperCase();
+  return arg1;
+
+}
+
+function roundDown(arg) {
+  var arg1 = Math.floor(arg);
+  return arg1;
+
+}
+
+function theTruth() {
+
+  return true
+}
